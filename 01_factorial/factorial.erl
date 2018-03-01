7b3711f824c275983a6dec737678d6e6c5e84674
Bill Mitchell <wkmitch@gmail.com>
2018-02-28 16:13:45 -0800

    Add tail recursion to 01_factorial

--- 01_factorial/factorial.erl
+++ 01_factorial/factorial.erl
@@ -1,5 +1,10 @@
 -module(factorial).
--export([fac/1]).
+-export([fac/1, tail_fac/1]).
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
+
+% Tail recursion
+tail_fac(N) -> tail_fac(N, 1).
+tail_fac(0, Acc) -> Acc;
+tail_fac(N, Acc) when N > 0 -> tail_fac(N-1, N*Acc).
