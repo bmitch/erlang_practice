d6e406d25fd2440750c1894981d859e10000af2e
Bill Mitchell <wkmitch@gmail.com>
2018-02-28 16:17:59 -0800

    Add tail recursion to 02_listlength

--- 02_length_of_a_list/listlength.erl
+++ 02_length_of_a_list/listlength.erl
@@ -1,6 +1,11 @@
 -module(listlength).
--export([len/1]).
+-export([len/1, tail_len/1]).
 
 len([]) -> 0;
 len([_]) -> 1;
 len([_|T]) -> 1 + len(T).
+
+% Tail recursion
+tail_len(L) -> tail_len(L, 0).
+tail_len([], Acc) -> Acc;
+tail_len([_|T], Acc) -> tail_len(T, Acc+1).
